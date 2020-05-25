6a293fb99d694f9a57112aa5b5c5f4540141da14
Akshaya-rithu <akshaya02rithu@gmail.com>
2020-05-25 18:55:22 +0530

    cloned and added code

--- src/org/hcl/test/M.java
+++ src/org/hcl/test/M.java
@@ -5,5 +5,11 @@ import java.io.IOException;
 public class M {
 	void method() throws IOException {
 		throw new IOException("device error");
+		
 	}
+	public static void main(String[] args) {
+		System.out.println("Add my exception code");
+
+	}
+		
 }
