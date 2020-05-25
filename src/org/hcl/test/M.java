9850755a02ed8f92e67f1bc81bcc648988c92564
Akshaya-rithu <akshaya02rithu@gmail.com>
2020-05-25 22:49:25 +0530

    Abinaya changed code

--- src/org/hcl/test/M.java
+++ src/org/hcl/test/M.java
@@ -5,5 +5,10 @@ import java.io.IOException;
 public class M {
 	void method() throws IOException {
 		throw new IOException("device error");
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("akali");
 	}
 }
